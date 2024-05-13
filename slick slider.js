74fb76d6def4c517d15994fd6ef8d911403e87cb
Tanay634 <145247230+Tanay634@users.noreply.github.com>
2024-05-13 23:54:04 +0530

    Add files via upload

---
+++ slick slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.slickslider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots:true,
+        arrows:true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 600,
+        cssEase: 'linear',
+    });
+});
